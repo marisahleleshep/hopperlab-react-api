ed9f50c2f82ab474adb3823eec229f492a952b38
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-07-04 21:57:38 +0300

    first commit

---
+++ src/Product/index.js
@@ -0,0 +1,36 @@
+import React, {useEffect,useState} from "react";
+
+const Product =()=>{
+    const [products,setProduct]=useState([]);
+    const [loading,setLoading]=useState(false);
+    useEffect(()=>{
+        (async()=>{
+            await getProducts();
+        })()
+    },[])
+    console.log({products});
+    const getProducts=async() =>{
+        try{
+            const response=await fetch ('https://dummyjson.com/products')
+            const result =await response.json();
+            setProduct(result.products);
+        }
+        catch(error){
+            console.log(error.message);
+        }
+    }
+    if (loading){
+        return <h2>Loading...</h2>
+    }
+    return(
+        <div>
+            <h2>List of Products</h2>
+            {products.map(item=>(
+                <div key ={item.id}>
+                    <h3>{item.title}</h3>
+                </div>
+            ))}
+        </div>
+    )
+}
+export default Product;
